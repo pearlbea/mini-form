09596948d0ab038d5f633a6aac1c0f8631229756
Pearl <pearl@bendyworks.com>
2017-05-01 16:59:18 -0500

    Adds step 2 code

--- mini-form.js
+++ mini-form.js
@@ -4,6 +4,18 @@ class MiniForm extends HTMLElement {
     super();
   }
 
+  connectedCallback() {
+    this.innerHTML = this.template;
+  }
+
+  get template() {
+    return `
+      <div>
+        <input type="text" name="question" />
+        <button>Submit</button>
+      </div>
+    `;
+  }
 }
 
 customElements.define('mini-form', MiniForm);
