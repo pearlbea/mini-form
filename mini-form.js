4a7ff18c523cdf54972b48f9ca18076ede36d0c3
Pearl <pearl@bendyworks.com>
2017-05-04 10:56:36 -0500

    Adds event handling

--- mini-form.js
+++ mini-form.js
@@ -6,11 +6,74 @@ class MiniForm extends HTMLElement {
 
   connectedCallback() {
     this.initShadowDom();
+    this.addFocusListener();
+    this.addClickListener();
   }
 
   initShadowDom() {
     let shadowRoot = this.attachShadow({mode: 'open'});
     shadowRoot.innerHTML = this.template;
+  }
+
+  addFocusListener() {
+    this.input.addEventListener('focus', e => {
+      this.hideErrorMessage();
+    });
+  }
+
+  hideErrorMessage() {
+    this.errorMessage.className = 'error hide';
+  }
+
+  addClickListener() {
+  this.button.addEventListener('click', e => {
+    this.getUserInput();
+    });
+  }
+
+  getUserInput() {
+    this.input.value.length > 0 ? this.handleSuccess() : this.displayErrorMessage();
+  }
+
+  handleSuccess() {
+    // You could call a method to save the user's answer here
+    this.displaySuccessMessage();
+  }
+
+  displaySuccessMessage() {
+    this.card.innerHTML = this.message;
+  }
+
+  displayErrorMessage() {
+    this.errorMessage.className = 'error';
+  }
+
+  get input() {
+    return this.shadowRoot.querySelector('input');
+  }
+
+  get button() {
+    return this.shadowRoot.querySelector('button');
+  }
+
+  get card() {
+    return this.shadowRoot.querySelector('.mdl-card');
+  }
+
+  get message() {
+    // this could be a separate component and probably should be if you make it more complicated
+    return `
+      <div>
+        <div class="mdl-card__title">
+          <h4>Thank you.</h4>
+        </div>
+        <div class="mdl-card__supporting-text">We have received your complaint.</div>
+        <div class="mdl-card__actions"></div>
+      </div>
+    `;
+  }
+  get errorMessage() {
+    return this.shadowRoot.querySelector('.error');
   }
 
   get theme() {
@@ -32,6 +95,9 @@ class MiniForm extends HTMLElement {
       i {
         margin-right: 5px;
       }
+      .hide {
+        visibility: hidden;
+      }
     </style>
     <div class="mdl-card mdl-shadow--2dp">
       <header class="mdl-layout__header">
@@ -46,6 +112,7 @@ class MiniForm extends HTMLElement {
       </div>
       <div class="mdl-card__actions">
         <button class="mdl-button mdl-button--raised mdl-button--accent">Submit</button>
+        <div class="error hide">Don't you have something to say?</div>
       </div>
     </div>
     `;
